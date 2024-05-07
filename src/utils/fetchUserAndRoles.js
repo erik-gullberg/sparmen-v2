65a8f1c84ed7405b4d7ff7554554c81310dc8c5c
Erik Gullberg <erik.gullberg@devoteam.com>
2024-05-07 20:11:54 +0200

    Fix unauthenticated users not being able to access anything

--- src/utils/fetchUserAndRoles.js
+++ src/utils/fetchUserAndRoles.js
@@ -2,11 +2,16 @@ import { createClient } from "@/utils/supabase/server";
 export default async function fetchUser() {
   const supabase = createClient();
   const user = await supabase.auth.getUser();
-  const roles = await supabase
-    .from("role")
-    .select("is_editor")
-    .eq("user_id", user.data.user.id)
-    .single();
 
-  return { ...user, roles: roles.data };
+  if (user.data.user) {
+    const roles = await supabase
+      .from("role")
+      .select("is_editor")
+      .eq("user_id", user.data.user.id)
+      .single();
+
+    return { ...user.data, roles: roles.data };
+  } else {
+    return { ...user.data };
+  }
 }
