d45050457b8027e052291ab9fd993e25b0fce523
erik-gullberg <erik.gullberg1@inter.ikea.com>
2026-04-03 22:55:59 +0200

    Remove banner

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,6 @@ export default function Home() {
         alignItems: "center",
       }}
     >
-      <Banner />
       <SearchBar />
       <div className={styles.container}>
         <div className={styles.containerHeader}>
